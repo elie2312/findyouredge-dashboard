bd9de58417f4d60ba46de48b33f287841d35a8b3
elie2312 <elie.bannes12@gmail.com>
2025-10-15 15:46:17 +0200

    Simplify next.config.js for Vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_URL + '/api/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
